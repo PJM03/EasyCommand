5e3a59b8157546ce03f0361682204e79bccbd24a
pjm03 <pjm5129@cu.ac.kr>
2025-04-08 16:26:37 +0900

    ArgumentType 에서 Character 타입을 제외한 모든 원시타입 대응 상수 제공

--- src/main/java/com/github/pjm03/easycommand/typefixed/argument/ArgumentType.java
+++ src/main/java/com/github/pjm03/easycommand/typefixed/argument/ArgumentType.java
@@ -17,4 +17,13 @@ public class ArgumentType<T> {
     public String valueToString(T value) {
         return fromValue.apply(value);
     }
+
+    public static ArgumentType<String> STRING = new ArgumentType<>(String.class, s -> s, s -> s);
+    public static ArgumentType<Integer> INT = new ArgumentType<>(Integer.class, Integer::parseInt, Object::toString);
+    public static ArgumentType<Long> LONG = new ArgumentType<>(Long.class, Long::parseLong, Object::toString);
+    public static ArgumentType<Double> DOUBLE = new ArgumentType<>(Double.class, Double::parseDouble, Object::toString);
+    public static ArgumentType<Float> FLOAT = new ArgumentType<>(Float.class, Float::parseFloat, Object::toString);
+    public static ArgumentType<Boolean> BOOLEAN = new ArgumentType<>(Boolean.class, Boolean::parseBoolean, Object::toString);
+    public static ArgumentType<Byte> BYTE = new ArgumentType<>(Byte.class, Byte::parseByte, Object::toString);
+    public static ArgumentType<Short> SHORT = new ArgumentType<>(Short.class, Short::parseShort, Object::toString);
 }
